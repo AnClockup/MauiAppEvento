fdb6cefee65c2064c5f433d247297890648c0ea0
Anderson Varjão <123276112+AnClockup@users.noreply.github.com>
2024-11-17 01:06:30 -0300

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,53 @@
+﻿using MauiAppHotel.Models;
+
+namespace MauiAppHotel
+{
+    public partial class App : Application
+    {
+        public List<Quarto> lista_quartos = new List<Quarto>
+        {
+            new Quarto()
+            {
+                Descricao = "Premium",
+                ValorDiariaAdulto = 110.0,
+                ValorDiariaCrianca = 55.0
+            },
+            new Quarto()
+            {
+                Descricao = "Vip",
+                ValorDiariaAdulto = 80.0,
+                ValorDiariaCrianca = 40.0
+            },
+            new Quarto()
+            {
+                Descricao = "Camarote",
+                ValorDiariaAdulto = 50,
+                ValorDiariaCrianca = 25
+            },
+            new Quarto()
+            {
+                Descricao = "Pista",
+                ValorDiariaAdulto = 25,
+                ValorDiariaCrianca = 12.5
+            }
+
+        };
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new Views.ContratacaoHospedagem());
+        }
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            var window = base.CreateWindow(activationState);
+
+            window.Width = 400;
+            window.Height = 600;
+
+            return window;
+
+        }
+    }
+}
